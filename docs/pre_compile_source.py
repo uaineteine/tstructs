2a1a7560f9c1399f86d3eefd2b6f83836c28189f
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-09-14 00:08:31 +1000

    update documentation

--- docs/pre_compile_source.py
+++ docs/pre_compile_source.py
@@ -7,9 +7,12 @@ from uainepydat import fileio
 from uainepydat import datatransform
 
 relative_directory = "../tstructs"  # Replace with your relative directory path
+
 python_files = fileio.list_files_of_extension(os.path.abspath(relative_directory), "py")
 modules = [os.path.splitext(os.path.basename(filepath))[0] for filepath in python_files]
-#sort these modules out in alphabetical order
+# Remove __init__ from the module list
+modules = [m for m in modules if m != "__init__"]
+# Sort these modules out in alphabetical order
 modules = sorted(modules)
 
 print("Python modules identified in the directory: " + relative_directory)
@@ -27,15 +30,6 @@ rst_lines = list(map(rst_text, modules))
 pre_compile_path = "source/index.rst_pre"
 pre_str = fileio.read_file_to_string(pre_compile_path)
 post_str = datatransform.replace_between_tags(pre_str, "automodule", rst_lines, deleteTags=True)
-
-#update the dependency list
-requirements_path = "../requirements.txt"
-#make these into dotpoints
-requirements = fileio.read_file_to_string(requirements_path)
-requirements = datatransform.break_into_lines(requirements)
-requirements = list(map(lambda string: datatransform.add_prefix(string, "* "), requirements))
-requirements.append("\n")
-post_str = datatransform.replace_between_tags(post_str, "dependencies", requirements, deleteTags=True)
 
 purpose_path = "../meta/purpose.txt"
 pur = fileio.read_file_to_string(purpose_path)
